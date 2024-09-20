5ad419b5039d8832860b169206446a5988bf0e10
WildanFathir <wildanfathir612@gmail.com>
2024-09-20 20:42:07 +0700

    Refactor loading component to use a centralized loading UI

--- src/app/dashboard/(home)/airplanes/loading.tsx
+++ src/app/dashboard/(home)/airplanes/loading.tsx
@@ -4,8 +4,30 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-red-500" />
+    <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-10">
+      <div className="flex flex-col items-center bg-white p-8 rounded-lg shadow-lg w-80">
+        <p className="mb-6 text-xl font-semibold text-gray-700">Please wait</p>
+        <svg
+          className="animate-spin h-16 w-16 text-red-500"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+      </div>
     </div>
   );
 }
